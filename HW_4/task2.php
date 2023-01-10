959673b70ea21362acf43391c54596ec45d98a14
Краус Евгений Владимирович <evgeniykraus@gmail.com>
2023-01-10 20:16:03 +0700

    Убрал HTML теги и перенос строк

--- HW_4/task2.php
+++ HW_4/task2.php
@@ -1,18 +1,14 @@
-<pre>
 <?php
 $daysBeforeExam = rand(0, 9);
 $daysToPrepareForExam = rand(0, 9);
-echo "Количество дней до экзамена: $daysBeforeExam\n";
+echo "Количество дней до экзамена: $daysBeforeExam. ";
 
-echo "Количество дней для подготовки к экзамену: $daysToPrepareForExam\n";
+echo $daysBeforeExam < $daysToPrepareForExam ? "Мне крышка. " : "Главное — не расслабляться. ";
 
-echo $daysBeforeExam < $daysToPrepareForExam ? "Мне крышка\n" : "Главное — не расслабляться\n";
+echo $daysBeforeExam > $daysToPrepareForExam ? "Ууу, ещё успею в дотку поиграть. " : "Успею или не успею. ";
 
-echo $daysBeforeExam > $daysToPrepareForExam ? "Ууу, ещё успею в дотку поиграть\n" : "Успею или не успею\n";
+echo $daysBeforeExam == $daysToPrepareForExam ? "Впритирочку. " : "Либо все плохо, либо очень плохо. ";
 
-echo $daysBeforeExam == $daysToPrepareForExam ? "Впритирочку" : "Либо все плохо, либо очень плохо";
-?>
-</pre>>
 
 
 

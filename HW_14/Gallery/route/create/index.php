cd5284c9b354b67ec6aabf65abfc85ac9a9ce688
Краус Евгений Владимирович <evgeniykraus@gmail.com>
2023-01-17 10:53:13 +0700

    Выполнил ДЗ №14 Удалил лишние картинки.

--- HW_14/Gallery/route/create/index.php
+++ HW_14/Gallery/route/create/index.php
@@ -1,3 +1,3 @@
 <?php
-include $_SERVER['DOCUMENT_ROOT'] . '/templates/header.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/templates/header.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/src/upload_img.php';

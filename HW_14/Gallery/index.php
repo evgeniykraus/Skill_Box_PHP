f0ec124a3c58aff311df79aa0f9dc9940e92fd4c
Краус Евгений Владимирович <evgeniykraus@gmail.com>
2023-01-17 10:50:22 +0700

    Выполнил ДЗ №14 Изменил расположение файлов.

--- HW_14/Gallery/index.php
+++ HW_14/Gallery/index.php
@@ -1,5 +1,5 @@
 <?php
-include $_SERVER['DOCUMENT_ROOT'] . '/templates/header.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/templates/header.php';
 ?>
 
 <form action="route/create/" method="get">
--- HW_14/Gallery/src/cutString.php
+++
@@ -1,9 +0,0 @@
-<?php
-function cutString($line, $length = 12, $appends = '...'): string
-{
-    if (mb_strlen($line) > $length) {
-        return mb_substr($line, 0, $length) . $appends;
-    } else {
-        return $line;
-    }
-}

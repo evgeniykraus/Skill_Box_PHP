0a6ddb7ddfd911183d0a6d963f11f92e22809eb1
Краус Евгений Владимирович <evgeniykraus@gmail.com>
2023-01-19 11:39:30 +0700

    Выполнил ДЗ №17

---
+++ HW_17/class_data_base.php
@@ -0,0 +1,186 @@
+<?php
+
+class TestDataBase
+{
+    private $connect;
+
+    public function __construct($host, $user, $password, $dbname)
+    {
+        $this->connect = new mysqli($host, $user, $password, $dbname);
+    }
+
+    /**
+     * Добавить новый класс животных
+     * @param string $name
+     * @param int $can_flying
+     * @return bool
+     * @throws Exception
+     */
+    public function addAnimalClass(string $name, int $can_flying): bool
+    {
+        $query = "INSERT INTO animal_classes (name, can_flying) VALUES (?, ?)";
+        $stmt = $this->connect->prepare($query);
+        if (!$stmt) {
+            throw new Exception("Error: " . $this->connect->error);
+        }
+        $stmt->bind_param("si", $name, $can_flying);
+        $result = $stmt->execute();
+        if (!$result) {
+            throw new Exception("Error: " . $stmt->error);
+        }
+        $stmt->close();
+        return $result;
+    }
+
+
+    /**
+     * Добавить новое животное в БД
+     * @param string $name
+     * @param int $can_flying
+     * @param int $legs_count
+     * @return bool
+     * @throws Exception
+     */
+    public function addAnimal(string $name, int $can_flying, int $legs_count): bool
+    {
+        $query = "INSERT INTO animals (name, can_flying, legs_count, class_id) VALUES (?, ?, ?, 1)";
+        $stmt = $this->connect->prepare($query);
+        if (!$stmt) {
+            throw new Exception("Error: " . $this->connect->error);
+        }
+        $stmt->bind_param("sii", $name, $can_flying, $legs_count);
+        $result = $stmt->execute();
+        if (!$result) {
+            throw new Exception("Error: " . $stmt->error);
+        }
+        $stmt->close();
+        return $result;
+    }
+
+    /**
+     * Получить информацию из таблицы
+     * @param string $tableName
+     * @return array
+     * @throws Exception
+     */
+    public function getAllFromTable(string $tableName): array
+    {
+        $query = "SELECT * FROM $tableName";
+        if ($result = $this->connect->query($query)) {
+            return $result->fetch_all();
+        } else {
+            throw new Exception("Error: " . $this->connect->error);
+        }
+    }
+
+
+    /**
+     * Добавить новую страну в БД
+     * @param string $name
+     * @param string $code
+     * @return bool
+     * @throws Exception
+     */
+
+    public function addCountry(string $name, string $code): bool
+    {
+        $query = "INSERT INTO countries (name, code) VALUES (?, ?)";
+        $stmt = $this->connect->prepare($query);
+        if (!$stmt) {
+            throw new Exception("Error: " . $this->connect->error);
+        }
+        $stmt->bind_param("si", $name, $code);
+        $result = $stmt->execute();
+        if (!$result) {
+            throw new Exception("Error: " . $stmt->error);
+        }
+        $stmt->close();
+        return $result;
+    }
+
+    /**
+     * Добавить новый город в БД
+     * @param string $name
+     * @param string $founded_at
+     * @param int $country_id
+     * @return bool
+     * @throws Exception
+     */
+    public function addCity(string $name, string $founded_at, int $country_id): bool
+    {
+        $query = "INSERT INTO cities (name, founded_at, country_id) VALUES (?, str_to_date(?, '%d.%m.%Y'), ?)";
+        $stmt = $this->connect->prepare($query);
+        if (!$stmt) {
+            throw new Exception("Error: " . $this->connect->error);
+        }
+        $stmt->bind_param("ssi", $name, $founded_at, $country_id);
+        $result = $stmt->execute();
+        if (!$result) {
+            throw new Exception("Error: " . $stmt->error);
+        }
+        $stmt->close();
+        return $result;
+    }
+
+
+    /**
+     * Обновит любую таблицу в БД
+     * @param string $table
+     * @param string $set
+     * @param string $newValue
+     * @param string $where
+     * @param string $equal
+     * @return bool
+     * @throws Exception
+     */
+    public function updateTable(string $table, string $set, string $newValue, string $where, string $equal): bool
+    {
+        $query = "UPDATE $table SET $set = ? WHERE $where = ?";
+        $stmt = $this->connect->prepare($query);
+        if (!$stmt) {
+            throw new Exception("Error: " . $this->connect->error);
+        }
+        $stmt->bind_param("ss", $newValue, $equal);
+        $result = $stmt->execute();
+        if (!$result) {
+            throw new Exception("Error: " . $stmt->error);
+        }
+        $stmt->close();
+        return $result;
+    }
+
+    public function deleteRow(string $table, string $where, string $equal): bool
+    {
+        $query = "DELETE FROM $table WHERE $where = ?";
+        $stmt = $this->connect->prepare($query);
+        if (!$stmt) {
+            throw new Exception("Error: " . $this->connect->error);
+        }
+        $stmt->bind_param("s", $equal);
+        $result = $stmt->execute();
+        if (!$result) {
+            throw new Exception("Error: " . $stmt->error);
+        }
+        $stmt->close();
+        return $result;
+    }
+
+    /**
+     * Выполнить любой запрос
+     * @param string $query
+     * @return array
+     * @throws Exception
+     */
+    public function query(string $query): array
+    {
+        if ($result = $this->connect->query($query)) {
+            if($result->num_rows > 0){
+                return $result->fetch_all();
+            }else{
+                return array();
+            }
+        } else {
+            throw new Exception("Error: " . $this->connect->error);
+        }
+    }
+}

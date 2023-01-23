ca3357236c96c06bc94585211cf4208cacb80778
Краус Евгений Владимирович <evgeniykraus@gmail.com>
2023-01-23 15:55:09 +0700

    Исправление замечаний: 1. Поправил, теперь констрейн для каждой таблицы свой. 2. Все сделал в ловеркейс

--- HW_16/create_table.sql
+++ HW_16/create_table.sql
@@ -1,39 +1,39 @@
--- Создание таблицы 'countries'
-CREATE TABLE countries (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL COMMENT 'название страны',
-    code CHAR(3) NOT NULL COMMENT 'символьный код страны',
-    PRIMARY KEY (id),
-    CONSTRAINT constraint_animal_classes UNIQUE (name)
-)COMMENT 'Таблица стран';
+-- создание таблицы 'countries'
+create table countries (
+    id int not null auto_increment,
+    name varchar(255) not null comment 'название страны',
+    code char(3) not null comment 'символьный код страны',
+    primary key (id),
+    constraint unique_countries_class unique (name)
+)comment 'таблица стран';
 
--- Создание таблицы 'cities'
-CREATE TABLE cities (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL COMMENT 'название города',
-    founded_at DATETIME COMMENT 'дата основания города',
-    country_id INT NOT NULL COMMENT 'ID страны, в которой находится этот город',
-    PRIMARY KEY (id),
-    FOREIGN KEY (country_id) REFERENCES countries (id)
-)COMMENT 'Таблица городов';
+-- создание таблицы 'cities'
+create table cities (
+    id int not null auto_increment,
+    name varchar(255) not null comment 'название города',
+    founded_at datetime comment 'дата основания города',
+    country_id int not null comment 'id страны, в которой находится этот город',
+    primary key (id),
+    foreign key (country_id) references countries (id)
+)comment 'таблица городов';
 
--- Создание таблицы 'animal_classes'
-CREATE TABLE animal_classes (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL COMMENT 'название класса животных: млекопитающее, рыба, птица и тому подобное',
-    can_flying TINYINT(1) DEFAULT 0 COMMENT 'признак — бывают ли среди них те, кто может летать',
-    PRIMARY KEY (id),
-    CONSTRAINT constraint_animal_classes UNIQUE (name)
-) COMMENT  'Таблица классов животных';
+-- создание таблицы 'animal_classes'
+create table animal_classes (
+    id int not null auto_increment,
+    name varchar(255) not null comment 'название класса животных: млекопитающее, рыба, птица и тому подобное',
+    can_flying tinyint(1) default 0 comment 'признак — бывают ли среди них те, кто может летать',
+    primary key (id),
+    constraint unique_animal_class unique (name)
+) comment  'таблица классов животных';
 
--- Создание таблицы 'animals'
-CREATE TABLE animals (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL COMMENT 'название животного: хрюшка, кот, черепаха и тому подобные',
-    can_flying TINYINT(1) DEFAULT 0 COMMENT 'признак — умеют ли летать',
-    legs_count INT COMMENT 'количество лап',
-    class_id INT NOT NULL COMMENT 'ID класса животного',
-    PRIMARY KEY (id),
-    FOREIGN KEY (class_id) REFERENCES animal_classes (id)
-)COMMENT  'Таблица животных';
+-- создание таблицы 'animals'
+create table animals (
+    id int not null auto_increment,
+    name varchar(255) not null comment 'название животного: хрюшка, кот, черепаха и тому подобные',
+    can_flying tinyint(1) default 0 comment 'признак — умеют ли летать',
+    legs_count int comment 'количество лап',
+    class_id int not null comment 'id класса животного',
+    primary key (id),
+    foreign key (class_id) references animal_classes (id)
+)comment  'таблица животных';
 
--- HW_19/tables/seed_tables.sql
+++ HW_19/tables/seed_tables.sql
@@ -1,19 +1,18 @@
 # Заполнение таблицы "groups"
-INSERT INTO test.GROUPS (NAME, DESCRIPTION)
-VALUES ('Зарегистрированный пользователь', 'Группа для зарегистрированных пользователей сайта'),
+insert into test.groups (name, description)
+values ('Зарегистрированный пользователь', 'Группа для зарегистрированных пользователей сайта'),
        ('Пользователи с правом писать сообщения',
         'Группа для пользователей, имеющих право писать сообщения на форуме сайта');
 
 # Заполнение таблицы "users"
-INSERT INTO test.USERS (ACTIVE, NAME, SURNAME, PATRONYMIC, EMAIL, PHONE, PASSWORD, EMAIL_NOTIFICATIONS)
-VALUES (1, 'Иван', 'Иванов', 'Иванович', 'ivan@example.com', '+71234567890', '$2y$10$/Op2bt66bmzOoU10ZrWV/OiUVWOaMRUPA94OkglK5Kx9sseSLJwmS', 1),
+insert into test.users (active, name, surname, patronymic, email, phone, password, email_notifications)
+values (1, 'Иван', 'Иванов', 'Иванович', 'ivan@example.com', '+71234567890', '$2y$10$/Op2bt66bmzOoU10ZrWV/OiUVWOaMRUPA94OkglK5Kx9sseSLJwmS', 1),
        (1, 'Петр', 'Петров', 'Петрович', 'petr@example.com', '+71234567891', '$2y$10$/Op2bt66bmzOoU10ZrWV/OiUVWOaMRUPA94OkglK5Kx9sseSLJwmS', 0),
        (1, 'Мария', 'Сидорова', 'Ивановна', 'maria@example.com', '+71234567892', '$2y$10$/Op2bt66bmzOoU10ZrWV/OiUVWOaMRUPA94OkglK5Kx9sseSLJwmS', 1);
 
 # Заполнение таблицы "group_user"
-INSERT INTO test.GROUP_USER (GROUP_ID, USER_ID)
-VALUES (1, 1),
+insert into test.group_user (group_id, user_id)
+values (1, 1),
        (1, 2),
-
        (1, 3),
        (2, 1);
